e00462e6a07133b8d6f1db4a90c25b913a3c60c5
princeofverry <verrykurniawan@students.undip.ac.id>
2024-07-30 06:13:08 +0700

    page dashboard done

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,26 +1,65 @@
-import React from "react";
+"use client";
+import React, { useState, ChangeEvent } from "react";
 import writing from "/public/images/writing.png";
 import Image from "next/image";
-import { Search } from "lucide-react";
+import { Pencil, Search, Trash, UserRound } from "lucide-react";
+import dummyNames from "@/lib/constant/name";
 
-const Dashboard = () => {
+interface NameItem {
+  name: string;
+}
+
+const Dashboard: React.FC = () => {
+  const [searchTerm, setSearchTerm] = useState<string>("");
+
+  const handleSearchChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredNames = dummyNames.filter((item: NameItem) =>
+    item.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div className="bg-[#D4D5D5] h-screen w-full px-4">
-      <div className="flex flex-row items-center justify-center md:space-x-56 space-x-12">
+      <div className="flex flex-row items-center justify-center md:space-x-56 space-x-8">
         <h1 className="font-bold md:text-4xl text-2xl">Hi, Admin!</h1>
         <Image src={writing} alt="alt" />
       </div>
       <div className="flex flex-col items-center">
         <div className="flex flex-col items-start">
-          <label className="text-xl">Search by name or number phone</label>
-          <div className="flex flex-col relative">
+          <label className="md:text-xl text-lg">
+            Search by name or number phone
+          </label>
+          <div className="flex flex-col relative mb-6 w-72">
             <input
-              type="email"
+              type="text"
               placeholder="Enter name"
+              value={searchTerm}
+              onChange={handleSearchChange}
               className="bg-blue rounded-xl px-12 py-2 border-2 border-[#3C5480] text-xl"
             />
             <Search className="absolute left-2 bottom-3" />
           </div>
+        </div>
+        <div className="h-60 overflow-y-auto no-scrollbar">
+          {filteredNames.map((item, index) => (
+            <div
+              key={index}
+              className="py-3 w-72 px-2 bg-[#BACC58] rounded-xl text-white mb-4"
+            >
+              <div className="flex flex-row justify-between">
+                <div className="flex flex-row justify-center gap-2">
+                  <UserRound />
+                  <h1>{item.name}</h1>
+                </div>
+                <div className="flex flex-row justify-center gap-2">
+                  <Pencil />
+                  <Trash />
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
---
+++ lib/constant/name.ts
@@ -0,0 +1,24 @@
+const dummyNames = [
+  { name: "Joko" },
+  { name: "Budi" },
+  { name: "Siti" },
+  { name: "Ahmad" },
+  { name: "Dewi" },
+  { name: "Rina" },
+  { name: "Putra" },
+  { name: "Agus" },
+  { name: "Rina" },
+  { name: "Yanto" },
+  { name: "Lina" },
+  { name: "Wati" },
+  { name: "Rudi" },
+  { name: "Sri" },
+  { name: "Teguh" },
+  { name: "Ani" },
+  { name: "Hendra" },
+  { name: "Maya" },
+  { name: "Andi" },
+  { name: "Yuni" },
+];
+
+export default dummyNames;
