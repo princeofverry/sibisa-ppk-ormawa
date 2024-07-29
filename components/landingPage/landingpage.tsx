8b8a4313bbf27ac7b192aeea1317d05c22f80d85
princeofverry <verrykurniawan@students.undip.ac.id>
2024-07-29 17:00:52 +0700

    first commit

---
+++ components/landingPage/landingpage.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const LandingPage = () => {
+  return (
+    <>
+      <div className="bg-[#D4D5D5] max-w-screen-sm h-screen">
+        <div className="text-center">
+          <h1 className="font-bold text-2xl">Innovative Waste Management</h1>
+          <h2 className="text-xl">IoT-Driven Waste Solutions</h2>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default LandingPage;
