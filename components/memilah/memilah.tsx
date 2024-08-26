4ed82489526355d25d9409c0eb7ee031b5517d54
princeofverry <verrykurniawan@students.undip.ac.id>
2024-08-26 11:21:54 +0700

    add:box + memilah

--- components/memilah/memilah.tsx
+++ components/memilah/memilah.tsx
@@ -6,6 +6,7 @@ import {
   AccordionTrigger,
 } from "@/components/ui/accordion";
 import accordionData from "@/lib/constant/accordion";
+import Box3R from "../box/box";
 
 const memilah = () => {
   return (
@@ -40,6 +41,36 @@ const memilah = () => {
             </AccordionItem>
           ))}
         </Accordion>
+      </div>
+      <div className="flex flex-col items-center text-[#3C5480]">
+        <h1 className="font-medium text-3xl text-center mt-8">
+          Memilah Sampah <span className="font-bold">Anorganik</span>
+        </h1>
+        <p className="text-2xl text-pretty text-justify w-10/12 my-8">
+          SIBISA adalah program berbasis IoT yang dirancang dengan cinta dan
+          dedikasi untuk mengelola limbah di Desa Sriwulan. Aplikasi ini
+          memonitor pengelolaan limbah anorganik dan fermentasi pupuk kompos
+          dari limbah peternakan. Dilengkapi dengan kader SIBISA, program ini
+          menciptakan sistem pengelolaan limbah yang terintegrasi dan efektif,
+          memberi manfaat material dan lingkungan bagi masyarakat.
+        </p>
+        <p className="text-2xl text-pretty text-justify w-10/12 mb-8">
+          Sampah anorganik yang telah dipilah dapat diolah dengan prinsip 3R
+          (reuse, reduce, dan recycle)
+        </p>
+      </div>
+      <Box3R />
+      <div className="z-10 relative mt-20 flex flex-col items-center text-[#3C5480] space-y-8 ">
+        <h1 className=" text-4xl font-medium">
+          Pengolahan Limbah Organik Atau{" "}
+          <span className="font-bold">Fermentasi Pupuk</span>
+        </h1>
+        <p className="w-10/12 text-justify text-2xl tracking-wide">
+          Desa Sriwulan memiliki limbah organik berupa kotoran sapi yang cukup
+          menjadi perhatian khusus bagi masyarakat setempat. Penumpukan limbah
+          kotoran sapi ternyata menyimpan potensi besar dengan kita alih
+          fungsikan menjadi pupuk yang bermanfaat untuk pertumbuhan tanaman.
+        </p>
       </div>
     </>
   );
