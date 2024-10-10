2910f52ce866df18531005586d0a048867d86df6
princeofverry <verrykurniawan@students.undip.ac.id>
2024-10-10 16:44:55 +0700

    add marker

--- components/denah/denah.tsx
+++ components/denah/denah.tsx
@@ -7,11 +7,12 @@ const Denah: React.FC = () => {
         Titik Rumah <span className="font-bold">Digital</span>
       </h1>
       <div className="flex flex-col items-center mt-8">
-        <div className="bg-white md:px-36 px-4 py-8 rounded-2xl shadow-2xl w-full h-[500px]"> {/* Adjust size accordingly */}
+        <div className="bg-white md:px-36 px-4 py-8 rounded-2xl shadow-2xl w-full h-[500px]">
           <iframe
             className="h-full w-full border-0"
             frameBorder="0"
-            src="https://www.google.com/maps/embed/v1/place?q=sriwulan+kendal&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
+            src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8
+            &q=-7.149576801329801,110.29685356062552&zoom=15`}
             allowFullScreen
           ></iframe>
         </div>
