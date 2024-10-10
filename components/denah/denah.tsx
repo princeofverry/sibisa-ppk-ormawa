ca8867610fdd86d51ebcef03f5ed19cd4ad4a36c
princeofverry <verrykurniawan@students.undip.ac.id>
2024-10-10 16:38:26 +0700

    change danah

--- components/denah/denah.tsx
+++ components/denah/denah.tsx
@@ -1,33 +1,6 @@
 import React from "react";
-import dynamic from "next/dynamic"; // Import dynamic from Next.js
-import "leaflet/dist/leaflet.css"; // Import CSS for Leaflet
-import L, { Icon } from "leaflet";
-
-// Custom marker icon
-const markerIcon: Icon = new L.Icon({
-  iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-});
-
-// Dynamically import the MapContainer with SSR disabled
-const MapContainer = dynamic(() => import("react-leaflet").then((mod) => mod.MapContainer), {
-  ssr: false,
-});
-const TileLayer = dynamic(() => import("react-leaflet").then((mod) => mod.TileLayer), {
-  ssr: false,
-});
-const Marker = dynamic(() => import("react-leaflet").then((mod) => mod.Marker), {
-  ssr: false,
-});
-const Popup = dynamic(() => import("react-leaflet").then((mod) => mod.Popup), {
-  ssr: false,
-});
 
 const Denah: React.FC = () => {
-  const position: [number, number] = [-7.149576801329801, 110.29685356062552]; // Location coordinates
-
   return (
     <>
       <h1 className="text-[#3C5480] md:text-3xl text-xl mt-4 font-medium text-center">
@@ -35,22 +8,12 @@ const Denah: React.FC = () => {
       </h1>
       <div className="flex flex-col items-center mt-8">
         <div className="bg-white md:px-36 px-4 py-8 rounded-2xl shadow-2xl w-full h-[500px]"> {/* Adjust size accordingly */}
-          <MapContainer 
-            center={position} 
-            zoom={14} 
-            scrollWheelZoom={false} 
-            style={{ height: "100%", width: "100%" }}
-          >
-            <TileLayer
-              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            <Marker position={position} icon={markerIcon}>
-              <Popup>
-                Titik Rumah Digital di Sriwulan, Kendal.
-              </Popup>
-            </Marker>
-          </MapContainer>
+          <iframe
+            className="h-full w-full border-0"
+            frameBorder="0"
+            src="https://www.google.com/maps/embed/v1/place?q=sriwulan+kendal&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
+            allowFullScreen
+          ></iframe>
         </div>
       </div>
     </>
